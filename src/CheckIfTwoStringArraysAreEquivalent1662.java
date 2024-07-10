de60d8971fb390b7dd2c163c1b2887c377f257a3
doantam123 <doantampn2017@gmail.com>
2024-07-11 03:28:47 +0700

    new assignment 1107

---
+++ src/CheckIfTwoStringArraysAreEquivalent1662.java
@@ -0,0 +1,31 @@
+public class CheckIfTwoStringArraysAreEquivalent1662 {
+    // public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+    //     String s1 = "";
+    //     String s2 = "";
+
+    //     for(String s : word1){
+    //         s1+=s;
+    //     }
+    //     for(String s : word2){
+    //         s2+=s;
+    //     }
+    //     return s1.equals(s2);
+    // }
+
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+
+        for(String s : word1){
+            s1.append(s);
+        }
+        for(String s : word2){
+            s2.append(s);
+        }
+        return s1.toString().equals(s2.toString());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/RichestCustomerWealth1672.java
@@ -0,0 +1,29 @@
+public class RichestCustomerWealth1672 {
+    public static int maximumWealth(int[][] accounts) {
+        int soKhachHang = accounts.length;
+        int soTKNganHang = accounts[0].length;
+        int[] tongTaiSan = new int[soKhachHang];
+
+        for (int i = 0; i < soKhachHang; i++) {
+            int tongTien = 0;
+            for (int j = 0; j < soTKNganHang; j++) {
+                tongTien += accounts[i][j];
+            }
+            tongTaiSan[i] = tongTien;
+        }
+
+        int max = 0;
+        for (int j = 0; j < soKhachHang; j++) {
+            if(tongTaiSan[j] > max)
+            {
+                max = tongTaiSan[j];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int [][] a =  {{1,2,3},{3,2,1}};
+        System.out.println(maximumWealth(a));      
+    }
+}
